fcd91413ad5e0989fb951f79330bf0b648c5ce1f
YeeunHeo <gjdpdms2005@gmail.com>
2020-03-04 00:00:17 +0900

    commit

--- yeeun/dp/1912.cpp
+++ yeeun/dp/1912.cpp
@@ -1,7 +1,30 @@
-// 미완성
 #include <iostream>
 using namespace std;
 
 int main(void){
+    int N;
+    int a[100010] = {};
+    int dp[100010] = {};
+    int max;
 
+    cin>>N;
+
+    for(int i = 1; i <= N; i++){
+        cin>>a[i];
+    }
+
+    for(int i = 1; i <= N; i++){
+        if(a[i] < dp[i-1] + a[i]){
+            dp[i] = dp[i-1] + a[i];
+        } else {
+            dp[i] = a[i];
+        }
+    }   
+
+    max = dp[1];
+    for(int i = 2; i <= N; i++){
+        if(max<dp[i]) max = dp[i];
+    }
+
+    cout<<max;   
 }
