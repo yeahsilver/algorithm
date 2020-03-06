7efcacfe59da54f495ae8436ed3a595dc39b53b9
yeeun <39258902+yeahsilver@users.noreply.github.com>
2020-03-06 11:32:54 +0900

    commit

---
+++ yeeun/sort/10825.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+pair<pair<string, int> ,pair<int, int> > v[100001];
+
+bool cmp(pair<pair<string, int> ,pair<int, int> > a,pair<pair<string, int> ,pair<int, int> > b){
+    if (a.first.second > b.first.second) return true;
+    else if(a.first.second == b.first.second){
+        if(a.second.first < b.second.first) return true;
+        else if(a.second.first == b.second.first){
+            if(a.second.second > b.second.second) return true;
+            else if(a.second.second == b.second.second){
+                if(a.first.first < b.first.first) return true;
+            }
+        }
+    }
+    return false;
+}
+
+int main(void){
+    int N;
+    cin>>N;
+
+    
+    for(int i = 0; i < N; i++){
+        cin>>v[i].first.first>>v[i].first.second>>v[i].second.first>>v[i].second.second;
+    }
+
+    sort(v, v + N, cmp);   
+
+    for(int i = 0; i< N; i++){
+        cout<<v[i].first.first<<"\n";
+    } 
+}
