224ccab156848e176ac99870a2802b3d31fe84c7
YeeunHeo <gjdpdms2005@gmail.com>
2020-03-14 18:01:18 +0900

    commit

--- yeeun/sort/1168.cpp
+++ yeeun/sort/1168.cpp
@@ -1,36 +1,31 @@
-// 미완성 
-
 #include <iostream>
 #include <vector>
 using namespace std;
 
 int main(void){
-	int N, K;
-	cin >> N, K;
-	vector<int> v;
-	
-	for(int i = 1; i <= N; i++){
-		v.push_back(i);	
-	}  
-	
-	int remove = K-1;
-	int copyN = N;
-	
-	cout<<"< ";
-	
+	int N, k;
+	cin >> N >> k;
+	vector<int> v(N);
+
 	for(int i = 0; i < N; i++){
-		cout<<v[remove];
-		if(i == N-1){
-			cout<<" >";
-		} else {
-			cout<<", ";
-		}
-		
-		v.erase(v.begin() + remove);
-		remove += (K - 1);
-		
-		if(--copyN > 0){
-			remove % copyN;
-		}
+		v[i] = i+1;
 	}
+
+	int index = 0;
+	vector<int> vec;
+
+	while(!v.empty()){
+		index = (index + (k-1)) % v.size();
+		vec.push_back(v[index]);
+		v.erase(v.begin() + index);
+	}
+
+	cout<<"<";
+
+	for(int i = 0; i < N; i++){
+		cout<<vec[i];
+		if(i < N-1) cout<<", ";
+	}
+	cout<<">";
+
 }
