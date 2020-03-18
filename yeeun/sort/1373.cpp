605aa71c62923821c83b64243ccc4785a2f46ada
YeeunHeo <gjdpdms2005@gmail.com>
2020-03-18 16:38:35 +0900

    commit

--- yeeun/sort/1373.cpp
+++ yeeun/sort/1373.cpp
@@ -1,5 +1,3 @@
-//미완성
-
 #include <iostream>
 #include <string>
 
@@ -9,12 +7,11 @@ int main(void){
     string s;
     cin >> s;
     string str; 
-    if(s.size()%3 != 0){
-        for(int i = 1; i < s.size()%3; i++){
-            str.append("0");
-        }
-        str = str.append(s);
-    }
+    if (s.size() % 3 == 1) str.append("00");
+    else if (s.size() % 3 == 2) str.append("0");
+        
+    str = str.append(s);
+
     for(int i = 0; i< s.size(); i+=3){
         cout<<(str[i] -'0') * 4 + (str[i+1]-'0') * 2  + (str[i+2]-'0');
     }
