25bf49a5f5e27d5673d35c789724799d54b5e1eb
yeeun <39258902+yeahsilver@users.noreply.github.com>
2020-05-14 23:09:49 +0900

    Create 10989.cpp

---
+++ yeeun/Itda/4주차/10989.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int arr[10001];
+int main(void){
+	int N, n;
+	cin >> N;
+	
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+	
+	for(int i = 0; i < N; i++){
+		cin >> n;
+		arr[n]++;
+		}	
+		
+	for(int i = 1; i <= 10000; i++){
+		for(int j = 0; j < arr[i]; j++){
+			cout<<i<<"\n";
+			}
+		}
+	}
