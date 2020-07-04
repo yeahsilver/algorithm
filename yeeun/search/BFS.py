0df82578b7521b3d8fd2c2d5d54e666fe1761a6a
yeeun <39258902+yeahsilver@users.noreply.github.com>
2020-07-04 15:54:06 +0900

    Create BFS.py

---
+++ yeeun/search/BFS.py
@@ -0,0 +1,26 @@
+graph['A'] = ['B', 'C']
+graph['B'] = ['A', 'D']
+graph['C'] = ['A', 'G', 'H', 'I']
+graph['D'] = ['B', 'E', 'F']
+graph['E'] = ['D']
+graph['F'] = ['D']
+graph['G'] = ['C']
+graph['H'] = ['C']
+graph['I'] = ['C', 'J']
+graph['J'] = ['I']
+
+def BFS(graph, start_node):
+    visited = []
+    need_visit =[]
+    
+    need_visit.append(start_node)
+    
+    while need_visit:
+        node = need_visit.pop(0)
+        if node not in visited:
+            visited.append(node)
+            need_visit.extend(graph[node])
+            
+    return visited
+    
+BFS(graph, 'A')
