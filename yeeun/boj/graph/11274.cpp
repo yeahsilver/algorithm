6e2b777a57c3de15a14325ede1041bb30827493b
yeeun <39258902+yeahsilver@users.noreply.github.com>
2020-07-11 11:46:45 +0900

    Update 11274.cpp

--- yeeun/boj/graph/11274.cpp
+++ yeeun/boj/graph/11274.cpp
@@ -5,33 +5,44 @@
 
 using namespace std;
 
-void BFS(vector<int>* graph, bool* visited, int start_node){
+void BFS(vector<int>* graph, bool visited[], int start_node){
 	// 1. 첫 노드를 queue에 삽입
 	queue<int> need_visit;
 	need_visit.push(start_node);
+	visited[start_node] = true;
 	
-	// 2. 첫 노드를 저장하고, pop
-	int node = need_visit.front();
-	cout << "first node:" << node << '\n';
-	need_visit.pop();
-	
-	// 3. queue가 empty일 때 까지 반복문 돌리기
+	// 2. queue가 empty일 때 까지 반복문 돌리기
 	while(!need_visit.empty()){
+		// 3. 첫 노드를 저장하고, pop
+		int node = need_visit.front();
+		need_visit.pop();
+		
 		for(int i = 0; i < graph[node].size(); i++){
 		// 4. 다음 node를 저장하고, visited 여부 따지기
 			int next = graph[node][i];
 			
 			if(!visited[next]){
 				// 5. 만약 visited 하지 않았으면,need_visit에 노드를 추가한 뒤, visited = true 체크
-				cout <<"next node: " <<next << '\n';
 				need_visit.push(next);
 				visited[next] = true;	
 			}
-			cout << "next!" << '\n';
 		} 
 	}
 }
 
+int connect(int n, vector<int>* graph){
+	bool visited[n + 1];
+	int cnt = 0;
+	
+	for(int i = 1; i <= n; i++){
+		if(visited[i]) continue;
+		
+		BFS(graph, visited, i);
+		cnt++;
+	}
+	
+	return cnt;
+}
 
 int main(void){
 	cin.tie(NULL);
@@ -40,8 +51,7 @@ int main(void){
 	
 	// 변수 생성
 	int edges, vertices;
-	int cnt = 0;
-	bool visited[edges + 1];
+	
 
 	// 1. edges, vertices 받기
 	
@@ -60,7 +70,5 @@ int main(void){
 		graph[end].push_back(start);
 	}
 	
-	for(int i = 1; i <= edges; i++){
-		BFS(graph, visited, i);
-	}
+	cout << connect(edges, graph) << '\n';
 }
