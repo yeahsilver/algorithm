e292d7c3b14703cabe1bb0b4c6b92e7a3eac7c47
YeeunHeo <gjdpdms2005@gmail.com>
2020-07-12 14:00:51 +0900

    11724.cpp complete

--- yeeun/boj/graph/11724.cpp
+++ yeeun/boj/graph/11724.cpp
@@ -1,4 +1,3 @@
-// 미완성
 #include <iostream>
 #include <vector>
 #include <queue>
@@ -6,10 +5,17 @@
 using namespace std;
 
 void BFS(vector<int>* graph, bool visited[], int start_node){
+	/*
+		BFS는 queue의 구조를 사용하여 작동하기에, need_visit라는 queue를 생성하여 방문해야하는 노드를 저장해준다. 
+		- need_visit: 방문 예정인 노드
+		- visited: 방문한 노드
+		- start_node: 처음으로 받은 노드
+	*/
+
 	// 1. 첫 노드를 queue에 삽입
 	queue<int> need_visit;
-	need_visit.push(start_node);
 	visited[start_node] = true;
+	need_visit.push(start_node);
 	
 	// 2. queue가 empty일 때 까지 반복문 돌리기
 	while(!need_visit.empty()){
@@ -19,21 +25,33 @@ void BFS(vector<int>* graph, bool visited[], int start_node){
 		
 		for(int i = 0; i < graph[node].size(); i++){
 		// 4. 다음 node를 저장하고, visited 여부 따지기
-			int next = graph[node][i];
+			int next_node = graph[node][i];
 			
-			if(!visited[next]){
+			if(!visited[next_node]){
 				// 5. 만약 visited 하지 않았으면,need_visit에 노드를 추가한 뒤, visited = true 체크
-				need_visit.push(next);
-				visited[next] = true;	
+				need_visit.push(next_node);
+				visited[next_node] = true;	
 			}
 		} 
 	}
 }
 
-int connect(int n, vector<int>* graph){
-	bool visited[n + 1];
+int find_connect_factor(int n, vector<int>* graph){
+	// 1. 변수 할당
 	int cnt = 0;
+	bool visited[n + 1];
 	
+	// 2. 배열 초기화
+	// 아직 노드를 방문하지 않았기에, 모든 원소를 false로 할당.
+	for(int i = 0; i < n+1; i++){
+		visited[i] = false;
+	}
+	
+	/* 3. 연결 요소 찾기
+		> if | 노드를 이미 방문했다면, 이미 간선으로 연결되어있는 것이기에, 다음 노드로 넘어감
+		> else | 노드를 방문하지 않았다면, 간선으로 연결되어있지 않음을 나타내는 것이기에, BFS 함수를 사용하여 노드를 탐색한다. 
+		  그리고 문제에서 원하는 출력값은 연결 요소의 개수이므로, BFS가 동작할 때 마다 cnt를 1씩 증가시킨다. 
+	*/
 	for(int i = 1; i <= n; i++){
 		if(visited[i]) continue;
 		
@@ -41,6 +59,7 @@ int connect(int n, vector<int>* graph){
 		cnt++;
 	}
 	
+	// 4. 연결요소의 개수 반환
 	return cnt;
 }
 
@@ -52,15 +71,14 @@ int main(void){
 	// 변수 생성
 	int edges, vertices;
 	
-
 	// 1. edges, vertices 받기
-	
 	cin >> edges >> vertices;
 	
 	// 2. edges 개수만큼 배열 생성
 	vector<int> graph[edges+1];
 	
-	// 3. 연결되어있는 정점 연결
+	// 3. 연결되어있는 정점 연결 
+	// 방향성이 없는 그래프이기 때문에 start -> end, end -> start의 경우를 배열에 모두 저장해주어야함.
 	for(int i = 0; i < vertices; i++){
 		int start, end;
 		
@@ -70,5 +88,6 @@ int main(void){
 		graph[end].push_back(start);
 	}
 	
-	cout << connect(edges, graph) << '\n';
+	// 4. 연결 요소의 개수 출력
+	cout << find_connect_factor(edges, graph) << '\n';
 }
