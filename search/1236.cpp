0c94987fba4819d09d4b692ce47aab38038a0bcf
YeeunHeo <gjdpdms2005@gmail.com>
2020-08-13 20:50:09 +0900

    commit

--- search/1236.cpp
+++ search/1236.cpp
@@ -1,40 +1,40 @@
-#include <iostream>
+    #include <iostream>
 
-using namespace std;
+    using namespace std;
 
-int main(void){
-    int N, M;
-    int rowCnt = 0;
-    int columnCnt = 0;
-    char castle[50][50];
+    int main(void){
+        int N, M;
+        int rowCnt = 0;
+        int columnCnt = 0;
+        char castle[50][50];
 
-    cin >> N >> M;
+        cin >> N >> M;
 
-    int row[50] = {0,};
-    int column[50] = {0,};
+        int row[50] = {0,};
+        int column[50] = {0,};
 
-    for(int i = 0; i < N; i++){
-        for(int j = 0; j < M; j++){
-            cin >> castle[i][j];
+        for(int i = 0; i < N; i++){
+            for(int j = 0; j < M; j++){
+                cin >> castle[i][j];
 
-            if(castle[i][j] == 'X'){
-                row[i] = 1;
-                column[j] = 1;
+                if(castle[i][j] == 'X'){
+                    row[i] = 1;
+                    column[j] = 1;
+                }
             }
         }
-    }
 
-    for(int i = 0; i < N; i++){
-        if(row[i] == 0){
-            rowCnt++;
+        for(int i = 0; i < N; i++){
+            if(row[i] == 0){
+                rowCnt++;
+            }
         }
-    }
 
-    for(int j = 0; j < M; j++){
-        if(column[j] == 0){
-            columnCnt++;
+        for(int j = 0; j < M; j++){
+            if(column[j] == 0){
+                columnCnt++;
+            }
         }
-    }
 
-    cout << max(rowCnt, columnCnt) << '\n';
-}
+        cout << max(rowCnt, columnCnt) << '\n';
+    }
---
+++ search/1939.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+void BFS(int* visited){
+    queue<int> need_visit;
+
+
+}
+int main(void){
+    int N, M;
+    cin >> N >> M;
+
+    int* visited = new int[N+1];
+
+    BFS(visited);
+}
