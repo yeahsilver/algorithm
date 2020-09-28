78b5426b0fbae7b2b598e2ebeecd649cbb32210d
YeeunHeo <gjdpdms2005@gmail.com>
2020-09-28 12:40:41 +0900

    complete 7490.cpp

--- recursive/7490.cpp
+++ recursive/7490.cpp
@@ -1,22 +1,34 @@
 #include <iostream>
-#include <vector>
+#include <string>
+#include <typeinfo>
 
 using namespace std;
 
-void make_zero(){
+int value;
 
+void make_zero(int sum, int op, int num, int cnt, string str){
+    if(value == cnt){
+        sum += (num * op);
+        if(sum == 0){
+           cout << str << '\n';
+        }
+    } else {
+        cnt++;
+        string str1 = to_string(cnt);
+    
+        make_zero(sum, op, (num*10)+cnt, cnt, str+" "+ str1);
+        make_zero(sum + (num * op), 1, cnt, cnt, str+"+"+str1);
+        make_zero(sum + (num * op), -1, cnt, cnt, str+"-"+str1);
+    }
 }
 
 int main(void){
     int test_case;
-
     cin >> test_case;
 
     while(test_case--){
-        
-        make_zero();
+        cin >> value;
+        make_zero(0, 1, 1, 1, "1");
+        cout << '\n';
     }
-    
-
-
 }
