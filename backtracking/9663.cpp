90dde09aee50986e0f597b1ee007aaa3c21f548a
YeeunHeo <gjdpdms2005@gmail.com>
2020-10-22 11:11:31 +0900

    complete 9663.cpp

--- backtracking/9663.cpp
+++ backtracking/9663.cpp
@@ -5,11 +5,15 @@
 using namespace std;
 
 int col[MAX];
-int N, result = 0;
+int N, unattackable = 0;
 
-bool promising(int location){
-    for(int i = 0; i < location; i++){
-        if(col[i] == col[location] || abs(col[location]- col[i]) == location-i){
+bool promising(int row){
+    for(int i = 0; i < row; i++){ // row만큼
+        if(col[i] == col[row] || abs(col[row]- col[i]) == row-i){
+            // col[i] == col[row] ==> 같은 column 선상에 있다
+            // abs(col[row] - col[i]) == row - i
+            // ==> (col[row], row), (col[i],i)일 때
+            // col[row]-col[i] = row -i 이면 같은 대각선 상에 존재 한다는 의미임
             return false;
         }
     }
@@ -18,12 +22,12 @@ bool promising(int location){
 
 void n_queens(int location){
     if(location == N){
-        result++;
+        unattackable++;
     } else {
         for(int i = 0; i < N; i++){
-            col[location] = i;
-            if(promising(location)){
-                n_queens(location+1);
+            col[location] = i; // 같은 행, 대각선, 아래 위로 말이 있는지 확인하기 위함
+            if(promising(location)){ // 같은 행, 대각선, 아래 위로 말이 없으면
+                n_queens(location+1); // 다음 줄로 넘어가서 체크하기!
             }
         }
     }
@@ -32,5 +36,5 @@ void n_queens(int location){
 int main(void){
     cin >> N;
     n_queens(0);
-    cout << result << '\n';
+    cout << unattackable << '\n';
 }
