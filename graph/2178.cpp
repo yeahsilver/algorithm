2884e18db71a2f83e4adb932ad11e5506de08b72
YeeunHeo <gjdpdms2005@gmail.com>
2020-10-26 23:24:16 +0900

    incomplete 2178.cpp

--- graph/2178.cpp
+++ graph/2178.cpp
@@ -1,53 +1,52 @@
 #include <iostream>
 #include <queue>
-#include <vector>
-
-using namespace std;
+#include <algorithm>
 
 #define MAX 101
+#define P pair<int, int> 
+
+using namespace std;
 
 int N, M;
 int maze[MAX][MAX];
 int dx[4] = {-1, 0, 1, 0};
 int dy[4] = {0, 1, 0, -1};
 
-void BFS(){
-    queue<pair<int,int> > need_visit;
-    need_visit.push(make_pair(1,1));
+void BFS(int x, int y){
+    queue<P> need_visit;
+    need_visit.push(make_pair(x,y));
 
     while(!need_visit.empty()){
-        pair<int, int> now = need_visit.front();
+        P now = need_visit.front();
         need_visit.pop();
 
         for(int i = 0; i < 4; i++){
             int nx = now.first + dx[i];
             int ny = now.second + dy[i];
 
-            if(nx >= 0 && ny >= 0 && nx < N && ny < M && maze[nx][ny] == 1){
-                need_visit.push(make_pair(nx, ny));
-                maze[nx][ny] = maze[now.first][now.second] + 1;
+            if(nx >=0 && ny >= 0 && nx < N && ny < M){
+                if(maze[nx][ny] == 1){
+                    need_visit.push(make_pair(nx,ny));
+                    maze[nx][ny] = maze[now.first][now.second]+1;
+                }
             }
         }
     }
 }
 
 int main(void){
+    cin.tie(NULL), cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
     cin >> N >> M;
 
-    for(int i = 1; i <= N; i++){
-        for(int j = 1; j <= M; j++){
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j < M; j++){
             scanf("%1d", &maze[i][j]);
         }
     }
 
-    BFS();
+    BFS(0,0);
 
-    for(int i = 1; i <= N; i++){
-        for(int j = 1; j <= M; j++){
-           cout << maze[i][j] << '\t';
-        }
-        cout << '\n';
-    }
-    
-    cout << maze[N][M];
+    cout << maze[N-1][M-1];
 }
