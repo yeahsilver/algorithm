717e7e2d26da020468b6cfa58eade77de1a23432
YeeunHeo <gjdpdms2005@gmail.com>
2020-12-17 23:55:58 +0900

    Resolve 1463.cpp

--- dp/1463-1.cpp
+++ dp/1463-1.cpp
@@ -15,24 +15,23 @@ int main(void)
     ios_base::sync_with_stdio(false);
 
     int N;
-    int dp[MAX];
+    int dp[MAX] = {};
 
     cin >> N;
 
-    dp[0] = dp[1] = 1;
+    dp[0] = dp[1] = 0;
 
     for (int i = 2; i <= N; i++)
     {
         dp[i] = dp[i - 1] + 1;
 
-        if (dp[i] % 3 == 0)
-        {
-            dp[i] = minimum(dp[i], dp[i / 3] + 1);
-        }
-
-        if (dp[i] % 2 == 0)
+        if (i % 2 == 0)
         {
             dp[i] = minimum(dp[i], dp[i / 2] + 1);
+        }
+        if (i % 3 == 0)
+        {
+            dp[i] = minimum(dp[i], dp[i / 3] + 1);
         }
     }
     cout << dp[N] << '\n';
