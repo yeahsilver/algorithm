37bc803f1313b80bc594031c95f7dd9016e450fa
yeeun <39258902+yeahsilver@users.noreply.github.com>
2021-01-01 16:51:31 +0900

    Create 11399.cpp

---
+++ greedy/11399.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+#define MAX 1001
+using namespace std;
+
+int main(void){
+	int N;
+	cin >> N;
+	
+	int* arr = new int(N);
+	
+	for(int i = 0; i < N; i++){
+		cin >> arr[i];
+	}
+	
+	sort(arr, arr+N);
+	
+	
+	for(int i = 1; i < N; i++){
+		arr[i] += arr[i-1];
+	}
+	
+	int result = 0;
+	
+	for(int i = 0; i < N; i++){
+		result += arr[i];
+	}
+	
+	cout << result << '\n';
+}
