572f9fa9f05dd66f5e4b28a1666353ea26b4577c
yeahsilver <gjdpdms2005@gmail.com>
2021-01-19 11:20:02 +0900

    solved 1000

--- 입출력/1000.cpp
+++ 입출력/1000.cpp
@@ -2,7 +2,9 @@
 using namespace std;
 
 int main(void){
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
     int x, y;
-    cin>>x>>y;
-    cout<<x+y;
+    cin >> x >> y;
+    cout << x+y;
 }
