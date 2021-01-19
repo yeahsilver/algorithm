c125792185092df6bec9a2b3d67e892f85ba571a
yeahsilver <gjdpdms2005@gmail.com>
2021-01-19 12:11:31 +0900

    solved 1330

---
+++ 입출력/1330.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+    int A, B;
+    cin >> A >> B;
+    
+    if(A > B) {
+        cout << '>' << '\n';
+    } else if (A < B) {
+        cout << '<' << '\n';
+    } else if (A == B) {
+        cout << "==" << '\n';
+    }
+}
