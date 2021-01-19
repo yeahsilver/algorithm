3333dac4859a15986f7ab4895df63fc81118bf46
yeahsilver <gjdpdms2005@gmail.com>
2021-01-19 11:23:29 +0900

    solved 10998

---
+++ 입출력/10998.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    int A, B;
+    cin >> A >> B;
+    cout << A * B << '\n';
+}
