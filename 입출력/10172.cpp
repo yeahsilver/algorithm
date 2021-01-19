e41e6eb1b09f771ce95fe4b720d3322d63a4817a
yeahsilver <gjdpdms2005@gmail.com>
2021-01-19 11:16:17 +0900

    solved 10172

---
+++ 입출력/10172.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(void){
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    cout << "|\\_/|" << '\n';
+    cout << "|q p|   /}" << '\n';
+    cout << "( 0 )\"\"\"\\" << '\n';
+    cout << "|\"^\"`    |" << '\n';
+    cout << "||_/=\\\\__|" << '\n';
+}
