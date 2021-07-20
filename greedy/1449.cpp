d97b18b9325745697513027f6e1a34ea1a5885da
yeahsilver <gjdpdms2005@gmail.com>
2021-07-21 02:16:32 +0900

    Solve 1449.cpp

--- greedy/1449.cpp
+++ greedy/1449.cpp
@@ -15,23 +15,59 @@ int main(void) {
         leaks.push_back(value);
     }
 
-    sort(leaks.begin(), leaks.end());
+    if(L == 1) {
+        cout << leaks.size() << '\n';
+        return 0;
+    }
 
-    int point = leaks.front();
-    int count = 0;
+    sort(leaks.begin(), leaks.end());
 
     L--;
 
-    for(int i = 0; i < leaks.size(); i++) {
-        int distance = leaks[i] - point;
+    int answer = 0;
+    int start = leaks.front();
+    int startIdx = 0;
 
-        if (distance >= L) {
-            count++;
-            point = leaks[i+1];
+    int j = 0;
 
-        } else if(distance == 0) {
-            count++;
-        } 
+    bool isFinished = false;
+
+    while(1) {
+
+        int tape = start + L;
+        int count = 0;
+    
+
+        // 탈출 조건 : 인덱스가 마지막 값을 가리킬 때 
+        if(j >= leaks.size()-1) {
+            if(!isFinished) {
+                answer++;
+            }
+            break;
+        }
+
+        for(j = startIdx; j < leaks.size(); j++) {
+            if(tape >= leaks[j]) {
+                count++; // 해당 값이 테이프에 포함할 수 있다면
+
+                if (leaks[j] == leaks.back()) { // 값이 테이프에 포함되는데 마지막 값이라면 미리 잘라주어야함.
+                    isFinished = true;
+                    break;
+                }
+            }
+
+            else if(tape < leaks[j]) { // 해당 값이 테이프에 포함할 수 없다면
+                startIdx = j;
+                start = leaks[j];
+                break;
+            }
+        }
+
+        if(count > 0) {
+            answer++;
+        }
+
     }
-    cout << count << '\n';
+
+    cout << answer << '\n';
 }
