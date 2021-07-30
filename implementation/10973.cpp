a1c84fb96119e8ce51694de4c9a74b8f673b6585
yeahsilver <gjdpdms2005@gmail.com>
2021-07-31 01:07:51 +0900

    Solve 10972.cpp and 10973.cpp

---
+++ implementation/10973.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main(void) {
+    int N;
+    cin >> N;
+
+    vector<int> v;
+    vector<int> prev;
+
+    for(int i = 1; i <= N; i++) {
+        int value;
+        cin >> value;
+        v.push_back(value);
+    }   
+
+   if(prev_permutation(v.begin(), v.end()) == true) {
+       for(int i = 0; i < N; i++) {
+           cout << v[i] << ' ';
+       }
+   } else {
+       cout << -1 << '\n';
+   }
+
+   return 0;
+}
