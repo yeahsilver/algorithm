cdb72af895b00b44ecb0f05f50cc3f6b4ee33cad
yeahsilver <gjdpdms2005@gmail.com>
2021-08-11 01:06:23 +0900

    Solve 9655.cpp

---
+++ dp/9655.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    int N;
+    cin >> N;
+
+    if(N%2 == 0) {
+        cout << "CY\n";
+    } else {
+        cout << "SK\n";
+    }
+}
