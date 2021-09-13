74c9dca3ca9784dcdffe446b0e69216540acb641
yeahsilver <gjdpdms2005@gmail.com>
2021-09-13 19:53:08 +0900

    Solve 2609.cpp

---
+++ math/2609.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int gcd(int a, int b) {
+    int c;
+
+    while(b != 0) {
+        c = a%b;
+        a = b;
+        b = c;
+    }
+
+    return a;
+}
+
+int lcm(int a, int b) {
+    return a * b / gcd(a,b);
+}
+int main(void) {
+    int a, b;
+    cin >> a >> b;
+
+    cout << gcd(a, b) << '\n';
+    cout << lcm(a, b) << '\n';
+}
