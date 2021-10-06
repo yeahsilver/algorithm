c005e8f2676e0afc955baa69a017a904f7ce0869
yeahsilver <gjdpdms2005@gmail.com>
2021-10-06 16:36:38 +0900

    Solve 1806.cpp

---
+++ two_pointer/1806.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+
+#define MAX 100001
+using namespace std;
+
+int main(void) {
+    int N, S;
+    cin >> N >> S;
+
+    vector<int> numbers;
+
+    for(int i = 0; i < N; i++) {
+        int value;
+        cin >> value;
+        numbers.push_back(value);
+    }
+
+    int left = 0;
+    int right = 0;
+    int sum = numbers[0];
+    int min = MAX;
+
+    while(1) {
+        if(right >= numbers.size()) {
+            break;
+        }
+
+        if(sum < S) {
+            if(right < numbers.size()) {
+                right++;
+                sum += numbers[right];
+            }
+
+        } else if (sum >= S){
+            int diff = right - left + 1;
+
+            if(min > diff) {
+                min = diff;
+            }
+
+            sum -= numbers[left];
+            left++;
+        }
+    }
+
+    min == MAX ? cout << 0 << '\n' : cout << min << '\n';
+}
