7a6af2d51fab074eaaefdce5642375d68fa63cbf
yeahsilver <gjdpdms2005@gmail.com>
2022-02-26 00:21:06 +0900

    solve 2018.cpp

--- 백준/implementation/2108-2.cpp
+++ 백준/implementation/2108-2.cpp
@@ -1,31 +1,66 @@
 #include <iostream>
-#include <vector>
 #include <algorithm>
+#include <vector>
+#include <cmath>
 
 using namespace std;
 
-int main(void) {
-    cin.tie(NULL);
-    ios_base::sync_with_stdio(false);
+#define MAX 500001
 
+int main(void) {
     int num;
     cin >> num;
 
-    vector<int> numbers;
+    int numbers[MAX];
+    int sum = 0;
+   
     for(int i = 0; i < num; i++) {
         int value;
-        numbers.push_back(value);
+        cin >> value;
+        sum += value;
+
+        numbers[i] = value;
     }
 
-    sort(numbers.begin(), numbers.end());
+    // 1. 산술 평균
+    float avg = (round(((float)sum)/num));
 
-    int sum = 0;
-
-    for (int number: numbers) {
-        sum+=number;
+    if (avg == 0) {
+        avg = abs(avg);
     }
 
-    cout << sum << '\n';
+    cout << avg << '\n';
 
-    
+    sort(numbers, numbers+num);
+
+    // 2. 중앙값
+    cout << numbers[(num-1)/2] << '\n';
+
+    // 3. 최빈값
+    int maximum = 0;
+    vector<int> v;
+
+    int index = 0;
+    while(1) {
+        if(index > num-1) {
+            break;
+        }
+
+        int value = upper_bound(numbers, numbers+num, numbers[index])-lower_bound(numbers, numbers+num, numbers[index]);
+       
+        if(maximum < value) {
+            maximum = value;
+            v.clear();
+            v.push_back(numbers[index]);
+        } else if(maximum == value) {
+            v.push_back(numbers[index]);
+        }
+
+        index += value;
+    }
+
+    v.size() > 1 ? cout << v[1] << '\n' : cout << v[0] << '\n';
+
+    // 4. 최댓값과 최소값의 차이
+    cout << numbers[num-1]-numbers[0] << '\n';
 }
